6d8f35b5bbe63e1e950e44255072a69e76cde08b
A R <A R@LAPTOP-M0TJ75U0>
2022-03-25 11:52:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "India", "Burma", "Australia", "Japan", "Sinapore" };
+            //var shortword = from word in arr where word.Length <=5 select word; //query syntax
+            var shortword = arr.Where(w => w.Length <= 5);//method syntax (lambda method)
+            foreach(var word in shortword)
+            {
+                Console.WriteLine(word);
+            }
+            Console.ReadLine();
+        }
+    }
+}
